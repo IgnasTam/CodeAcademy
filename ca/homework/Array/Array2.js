5add74df499c80980250ca8afc8224cea2995936
ignas <ignastamas@gmail.com>
2020-03-05 20:54:33 +0200

    Add all

--- ca/homework/Array/Array2.js
+++ ca/homework/Array/Array2.js
@@ -83,9 +83,9 @@ let arr6 = {
     Paulius:300,
     Mindaugas:300
 };
-var colours;
+var colour;
 for (n in arr6){
-    document.write(`${colours} - ${arr6[colours]} EUR <br>`);
+    document.write(`${colour} - ${arr6[colour]} EUR <br>`);
 }
 document.write(`<hr>`);
 // Duotas masyvas su elementais 2, 5, 9, 15, 0, 4.
